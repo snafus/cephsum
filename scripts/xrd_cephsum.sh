d4f038159ef1ad4cf580ee9d16fdc51e12319c9d
snafus <james.walder@gmail.com>
2021-06-18 20:10:22 +0100

    Update xrd_cephsum.sh
    Fix to use the exit code of the python command itself.

--- scripts/xrd_cephsum.sh
+++ scripts/xrd_cephsum.sh
@@ -4,9 +4,11 @@
 
 #Update the path name to the correct location
 RESULT=$(python3 /etc/xrootd/cephsum/cephsum.py -x /etc/xrootd/storage.xml -d --action=inget $1)
+ECODE=$(echo $?)
 
 # Additional logging could be added here if needed
 
 # Must return on stdout just the checksum followed by a newline 
 printf "${RESULT}\n"
-
+# Exit code from the output of the python script
+exit ${ECODE)
