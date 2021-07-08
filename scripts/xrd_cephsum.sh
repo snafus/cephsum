c62d85b016539694ced1f45f7887243154d3c84a
snafus <james.walder@gmail.com>
2021-07-08 13:00:20 +0100

    add -r

--- scripts/xrd_cephsum.sh
+++ scripts/xrd_cephsum.sh
@@ -3,7 +3,9 @@
 # If a timeout is explicitly requeired, could be included in the command below.
 
 #Update the path name to the correct location
-RESULT=$(python3 /etc/xrootd/cephsum/cephsum.py -x /etc/xrootd/storage.xml -d --action=inget $1)
+# -d enables debug logging (logging goes to the xrootd log file)
+# -r 64 implies to use 64MiB block size for each read request; see help for more info
+RESULT=$(python3 /etc/xrootd/cephsum/cephsum.py -x /etc/xrootd/storage.xml -d -r 64 --action=inget $1)
 ECODE=$(echo $?)
 
 # Additional logging could be added here if needed
