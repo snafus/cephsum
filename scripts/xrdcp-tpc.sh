4aa7d3ab289279beec7bad61ec73a481952ffb1f
snafus <james.walder@gmail.com>
2021-07-19 14:32:36 +0100

    xrdcp-tpc.sh updated to remove extra logging line
    Remove unnecessary logging

--- scripts/xrdcp-tpc.sh
+++ scripts/xrdcp-tpc.sh
@@ -3,8 +3,6 @@
 #Original code
 #/usr/bin/xrdcp --server -f $1 root://$XRDXROOTD_PROXY/$2
 
-echo "$@" >> /tmp/jw_xrdcp.log
-
 # Get the last two variables as SRC and DST, all others are assumed as additional arguments
 OTHERARGS="${@:1:$#-2}"
 DSTFILE="${@:$#:1}"
